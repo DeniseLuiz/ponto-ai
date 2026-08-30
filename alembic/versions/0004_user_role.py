ae3774ff072fb5350b30305febb10996390abd99
DeniseLuiz <denisesantos.luiz@gmail.com>
2026-08-30 10:55:41 -0300

    fix: alembic revision and update information

--- alembic/versions/0004_user_role.py
+++ alembic/versions/0004_user_role.py
@@ -1,3 +1,15 @@
+"""add user role
+
+Revision ID: 0004_user_role
+Revises: 0003_nome_da_migracao_anterior
+Create Date: 2026-08-30
+
+"""
+revision = "0004"
+down_revision = "0003"
+branch_labels = None
+depends_on = None
+
 from alembic import op
 import sqlalchemy as sa
 def upgrade():
